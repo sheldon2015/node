6d1edab52e7fc75242daa580ba3902e0500b8f9e
sheldon2015 <823231684@qq.com>
2017-02-03 09:50:11 +0800

    fix

--- src/senior_nodejs/stream/demo01.js
+++ src/senior_nodejs/stream/demo01.js
@@ -1,17 +1,14 @@
 var fs = require('fs');
 
-
 var writer = fs.createWriteStream('example.txt');
 writer.write('itbilu.com');
 //写入数据时，可以设置编码格式
 writer.write('aXRiaWx1LmNvbQ==', 'base64');
 
-
 //写缓冲区Buffer数据
 var buf = new Buffer('itbilu.com');
 writer.write(buf);
 writer.end('finish');
-
 
 //写入流   1.drain事件 2. finish事件
 writer.on('drain', () => {
